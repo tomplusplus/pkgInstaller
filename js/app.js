1b6b85e36cf4677ea1c2472b3d3f182a8db88bf7
Tom <tomplsuplsu>
2014-12-17 20:47:36 -0700

    Added function to add dependencies and chain dependencies

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
 /**
+ * Package Installer
  * Created by Tom on 12/17/14.
  */
 'use strict';
 
-//pkg installer code
+function PackageDependencyGraph(){
+    this.dependencies = {};
+    this.graphIndex = [];
+
+
+
+    //TODO write out packages in proper order
+}
+PackageDependencyGraph.prototype.addDependencies = function(pkg,dep){
+    this.graphIndex.push(pkg);
+    this.graphIndex.push(dep);
+    if (!this.dependencies.hasOwnProperty(dep)){
+        this.dependencies[dep] = [];
+    }
+    this.dependencies[dep].push(pkg);
+
+    //TODO find a way to chain dependencies together
+    //chain dependencies : recursion is fun!!! :)
+    if(typeof this.dependencies[pkg] !== 'undefined'){
+        this.dependencies[pkg].forEach(function(p){
+            this.addDependency(p,dep);
+        });
+    }
+};
+
+//TODO sort order of dependencies
+PackageDependencyGraph.prototype.orderDependencies = function(){
+
+};
